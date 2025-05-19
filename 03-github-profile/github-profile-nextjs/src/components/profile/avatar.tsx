18d92c07405d4d9e90f79278f06f10e0ac498dca
Ivan Herlan Herbas Zubieta <79385821+ivancidev@users.noreply.github.com>
2025-05-18 22:15:29 -0400

    fix: 🐛 Improve Avatar component to handle missing src and alt props gracefully; update InfoSocialProfile for better layout and styling 🎨

--- 03-github-profile/github-profile-nextjs/src/components/profile/avatar.tsx
+++ 03-github-profile/github-profile-nextjs/src/components/profile/avatar.tsx
@@ -8,9 +8,9 @@ export default function Avatar({ alt, src }: AvatarProps) {
   return (
     <div className="flex items-center justify-start">
       <Image
-        src={src}
+        src={src || "vercel.svg"}
         className="rounded-full border-gray-200 border-[1px] border-solid"
-        alt={alt}
+        alt={alt || "Avatar"}
         width={270}
         height={270}
         priority
--- 03-github-profile/github-profile-nextjs/src/components/profile/info-social-profile.tsx
+++ 03-github-profile/github-profile-nextjs/src/components/profile/info-social-profile.tsx
@@ -6,28 +6,23 @@ type Props = {
 export const InfoSocialProfile = ({ profile }: Props) => {
   return (
     <section>
-      {profile.bio && <p className="text-gray-500 text-sm">{profile.bio}</p>}
-      <div className="flex items-center gap-2">
+      <p className="text-gray-500 text-sm my-3">{profile.bio}</p>
+      <div className="flex items-center gap-2 my-3">
         <span>
           {/* icon */}
           <b>{profile.followers}</b> Followers <b>{profile.following}</b>{" "}
           Following
         </span>
       </div>
-      {profile.public_gists && (
-        <div className="flex items-center gap-2">
-          {/* icon */}
-          <span>{profile.public_gists}</span>
-        </div>
-      )}
+     
       {profile.location && (
-        <div className="flex items-center gap-2">
+        <div className="flex items-center gap-2 my-3">
           {/* icon */}
           <span>{profile.location}</span>
         </div>
       )}
       {profile.blog && profile.blog !== "" && (
-        <div className="flex items-center gap-2">
+        <div className="flex items-center gap-2 my3">
           {/* icon */}
           <a href={profile.blog} target="_blank" rel="noreferrer">
             <span>{profile.blog}</span>
