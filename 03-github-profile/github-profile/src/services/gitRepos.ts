d106d37aed5d7c70e74857f7d1126b0b64b21362
devferx <ferkki@hotmail.com>
2025-05-25 22:52:52 -0400

    Add environment configuration, update .gitignore, and refactor components for improved readability and functionality

--- 03-github-profile/github-profile/src/services/gitRepos.ts
+++ 03-github-profile/github-profile/src/services/gitRepos.ts
@@ -1,7 +1,10 @@
 export async function getGitRepos(username: string) {
     const api = process.env.NEXT_PUBLIC_GITHUB_API;
     const response = await fetch(`${api}/users/${username}/repos?page=1&sort=updated` ,{
-        next:{revalidate: 60},
+        next: { revalidate: 60 },
+        headers: {
+            'Authorization': `Bearer ${process.env.NEXT_PUBLIC_GITHUB_TOKEN}`
+        }
     });
     if(!response.ok) {
         throw new Error('Failed to fetch data');
