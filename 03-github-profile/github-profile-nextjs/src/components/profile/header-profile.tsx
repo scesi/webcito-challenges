b409f838a55315514bd1d957646a7c08b01af12a
Ivan Herlan Herbas Zubieta <79385821+ivancidev@users.noreply.github.com>
2025-05-25 21:22:38 -0400

    refactor(components): update styles and responsive

--- 03-github-profile/github-profile-nextjs/src/components/profile/header-profile.tsx
+++ 03-github-profile/github-profile-nextjs/src/components/profile/header-profile.tsx
@@ -1,20 +1,20 @@
-import { Roboto } from "next/font/google";
+import { Inter } from 'next/font/google';
 interface HeaderProfileProps {
   name: string;
   userName: string;
 }
 
-const roboto = Roboto({
-  weight: ["400"],
-  subsets: ["latin"],
+const roboto = Inter({
+  weight: ['400', '500', '600', '700'],
+  subsets: ['latin'],
 });
 export default function HeaderProfile({ name, userName }: HeaderProfileProps) {
   return (
     <header
       className={`${roboto.className} flex flex-col justify-start text-2xl mt-5 mb-5`}
     >
-      <h2 className="font-bold text-gray-200">{name}</h2>
-      <p className="text-gray-400 text-xl">{userName}</p>
+      <h2 className="font-semibold text-[26px] text-gray-200">{name}</h2>
+      <p className="text-gray-400 text-[20px] mt-2">{userName}</p>
     </header>
   );
 }
--- 03-github-profile/github-profile-nextjs/src/components/profile/info-social-profile.tsx
+++ 03-github-profile/github-profile-nextjs/src/components/profile/info-social-profile.tsx
@@ -1,29 +1,44 @@
-import { Profile } from "@/app/types/profile";
+import { Profile } from '@/app/types/profile';
+import { Inter } from 'next/font/google';
+import {
+  LocationUserIcon,
+  ProjectIcon,
+  TwitterIcon,
+  UsersIcon,
+} from '../icons';
 
 type Props = {
   profile: Profile;
 };
+
+const inter = Inter({
+  weight: ['400', '500', '600', '700'],
+  subsets: ['latin'],
+});
+
 export const InfoSocialProfile = ({ profile }: Props) => {
   return (
-    <section>
-      <p className="text-gray-500 text-sm my-3">{profile.bio}</p>
-      <div className="flex items-center gap-2 my-3">
-        <span>
-          {/* icon */}
-          <b>{profile.followers}</b> Followers <b>{profile.following}</b>{" "}
-          Following
+    <section className={`${inter.className} flex flex-col`}>
+      <p className="text-gray-400 text-[16px] my-3">{profile.bio}</p>
+      <div className="flex items-center">
+        <span className="flex items-center gap-2">
+          <UsersIcon className="w-5 h-5" />
+          <p>{profile.followers}</p>
+          <p className="text-gray-400">Followers</p>
+          · <p>{profile.following}</p>
+          <p className="text-gray-400">Following</p> ·
         </span>
       </div>
-     
+
       {profile.location && (
-        <div className="flex items-center gap-2 my-3">
-          {/* icon */}
-          <span>{profile.location}</span>
+        <div className="flex items-center gap-2 my-3 text-gray-400">
+          <LocationUserIcon className="w-5 h-5" />
+          <p>{profile.location}</p>
         </div>
       )}
-      {profile.blog && profile.blog !== "" && (
+      {profile.blog && profile.blog !== '' && (
         <div className="flex items-center gap-2 my3">
-          {/* icon */}
+          <ProjectIcon className="w-5 h-5" />
           <a href={profile.blog} target="_blank" rel="noreferrer">
             <span>{profile.blog}</span>
           </a>
@@ -31,7 +46,7 @@ export const InfoSocialProfile = ({ profile }: Props) => {
       )}
       {profile.twitter_username && (
         <div className="flex items-center gap-2">
-          {/* icon */}
+          <TwitterIcon className="w-5 h-5" />
           <a
             href={`https://twitter.com/${profile.twitter_username}`}
             target="_blank"
